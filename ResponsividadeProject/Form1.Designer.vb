3df635997308a7d6b7d64837e7e6e90e8b07cf42
paulohdiniz <ph.diniz@usp.br>
2020-06-14 06:11:31 -0300

    calculo da responsividade e grafico de vetor funcionando

---
+++ ResponsividadeProject/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.openFD = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'openFD
+        '
+        Me.openFD.FileName = "openFD"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1348, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(128, 26)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(173, 31)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(828, 629)
+        Me.Chart1.TabIndex = 1
+        Me.Chart1.Text = "Chart1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1209, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 126)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1348, 734)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents openFD As OpenFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ResponsividadeProject/Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim path As String
+        Dim text As String
+        Dim indice As Integer
+        Dim temporario As Double
+        Dim yInterpolado(0) As Double
+
+        Dim potencia(0) As Double
+        Dim responsividade(0) As Double
+
+
+        Dim columnXRef() As Double
+        Dim columnYRef() As Double
+
+        Dim columnXCalib() As Double
+        Dim columnYCalib() As Double
+
+        Dim columnXAmostra() As Double
+        Dim columnYAmostra() As Double
+
+
+        path = openFile()
+        text = readTxtComplete(path)
+        columnYRef = getColumYOfStringComplete(text) 'column y é a primeira coluna do txt
+        columnXRef = getColumXOfStringComplete(text)  'column x é a segunda coluna do txt
+
+        path = openFile()
+        text = readTxtComplete(path)
+        columnYCalib = getColumYOfStringComplete(text)
+        columnXCalib = getColumXOfStringComplete(text)
+
+        path = openFile()
+        text = readTxtComplete(path)
+        columnYAmostra = getColumYOfStringComplete(text)
+        columnXAmostra = getColumXOfStringComplete(text)
+
+        For i = LBound(columnXCalib) + 1 To UBound(columnXCalib)
+            indice = menorque(columnXCalib(i), columnXRef)
+
+            If (indice = -1) Then
+                temporario = columnXRef(columnXRef.Length - 1)
+                Add(Of Double)(yInterpolado, temporario)
+
+            Else
+                temporario = ((columnXCalib(i)) * (columnYRef(indice + 1) - columnYRef(indice)) + columnYRef(indice) * columnXRef(indice + 1) - columnXRef(indice) * columnYRef(indice + 1)) / (columnXRef(indice + 1) - columnXRef(indice))
+                Add(Of Double)(yInterpolado, temporario)
+            End If
+            Add(Of Double)(potencia, columnYCalib(i) / temporario)
+            Add(Of Double)(responsividade, (columnYAmostra(i) * temporario) / columnYCalib(i))
+
+        Next i
+        Console.WriteLine(columnXRef.Length)
+        Console.WriteLine(columnYRef.Length)
+        Console.WriteLine(columnXCalib.Length)
+        Console.WriteLine(columnYCalib.Length)
+        Console.WriteLine(columnXAmostra.Length)
+        Console.WriteLine(columnYAmostra.Length)
+        Console.WriteLine(potencia.Length)
+        Console.WriteLine(responsividade.Length)
+        Console.WriteLine(LBound(responsividade))
+        Console.WriteLine(UBound(responsividade))
+        For i = 0 To 89
+            Console.WriteLine(responsividade(i))
+        Next i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Dim outFile As IO.StreamWriter
+        Dim qlqr As String
+        outFile = IO.File.AppendText("C:\Users\phdin\Documents\text.txt")
+        For i = LBound(responsividade) + 1 To responsividade.Length - 1
+            qlqr = responsividade(i).ToString + " " + columnXAmostra(i).ToString
+            outFile.WriteLine(qlqr)
+            ' outFile.WriteLine(columnXAmostra(i))
+
+        Next i
+        outFile.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'setup the chart area 
+        Chart1.Titles.Add("Gráfico") 'specify chart name
+        Chart1.ChartAreas.Clear()
+        Chart1.ChartAreas.Add("Default")
+        With Chart1.ChartAreas("Default")
+            .AxisX.Title = "Comprimento de onda (nm)" 'x label
+            .AxisX.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.Title = "Responsividade" 'y label
+        End With
+        'specify series plot lines
+        Chart1.Series.Clear()
+        Chart1.Series.Add("plot1")
+        Chart1.Series("plot1").Color = Color.Red
+        Chart1.Series("plot1").ChartType = DataVisualization.Charting.SeriesChartType.Line
+
+        Dim path As String = openFile()
+        Text = readTxtComplete(path)
+        Dim y() As Double = getColumYOfStringComplete(Text)
+        Dim x() As Double = getColumXOfStringComplete(Text)
+
+        For i = 2 To x.Length - 1
+            Chart1.Series("plot1").Points.AddXY(x(i), y(i))
+        Next i
+    End Sub
+End Class
